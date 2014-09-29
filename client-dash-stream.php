77bb438f56f7327bfe298752078341dd82437ea1
Kyle Maurer <kyle@realbigmarketing.com>
2014-09-29 13:16:59 -0400

    Adding main file. Duh.

---
+++ client-dash-stream.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Client Dash Stream
+Plugin URI: http://clientdash.io
+Description: Brings the power of Stream into Client Dash.
+Version: 0.1
+Author: Kyle Maurer
+Author URI: http://kyleblog.net
+License: GPL2
+*/
